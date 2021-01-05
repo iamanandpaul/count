a58a954a241bf006789056ad609a81544badaa8d
iamanandpaul <iamanandpaul@gmail.com>
2021-01-05 11:43:51 +0530

    removed button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0,
+    };
+  }
+
+  handleIncrease = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  handleDecrease = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <h1>{this.state.number}</h1>
+        <div>
+          <button onClick={this.handleIncrease}>Increase</button>
+          <button onClick={this.handleDecrease}>Decrease</button>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
